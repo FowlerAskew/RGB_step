76ab82ff21d985e10ebd61dd82da4718df764a43
gremlin88 <mydisposable164@gmail.com>
2015-04-23 16:58:57 -0500

    Version 4
    Added step values

--- RGB_step.ino
+++ RGB_step.ino
@@ -3,20 +3,18 @@ const int input = 8;  //pin input number
 const int pin_R = 5;  //pin outputs
 const int pin_G = 6;
 const int pin_B = 7;
-int off_min = 0;  //min setting for all LEDs off
-int off_max = 0;  //max setting for all LEDs off
+int off_min = 900;  //min setting for all LEDs off
+int off_max = 1075;  //max setting for all LEDs off
 int first_min = off_max;  //mins and maxes for the first step
-int first_max = 0;
+int first_max = 1325;
 int second_min = first_max;  //min of the second step is the max of the first step
-int second_max = 0;  //second step min
+int second_max = 1500;  //second step max
 int third_min = second_max;  //same as the other steps
-int third_max = 0;
+int third_max = 1675;
 int fourth_min = third_max;
-int fourth_max = 0;
+int fourth_max = 1925;
 int fifth_min = fourth_max; 
-int fifth_max = 0;
-int sixth_min = fifth_max;
-int sixth_max = 0;
+int fifth_max = 2100;
 
 void setup()
 {
